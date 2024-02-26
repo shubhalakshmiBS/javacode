42b72e4a9397d9b6d690a54ecdd1592a71dcb2b2
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-02-26 14:53:43 +0530

    Added generated Unit test folder

---
+++ Unit_test/codes/newCode.java_test.java
@@ -0,0 +1,13 @@
+Unit Test Case:
+
+@Test
+public void testReverseString() {
+  String originalStr = "Hello";
+  String reversedStr = "";
+  for (int i = 0; i < originalStr.length(); i++) {
+    reversedStr = originalStr.charAt(i) + reversedStr;
+  }
+  assertEquals("olleH", reversedStr);
+}
+
+Code Coverage: 100%
